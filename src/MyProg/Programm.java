e5c8a0d580fb2ae3de7a7151fa6234c820b44a18
Kondyakov <letexa83@gmail.com>
2020-08-11 16:59:22 +0300

    Tak nado

--- src/MyProg/Programm.java
+++ src/MyProg/Programm.java
@@ -5,10 +5,14 @@ public class Programm {
         System.out.println("Hello world");
         System.out.println("Hello world");
         Book book = new Book();
+        System.out.println(book);
 
     }
 }
 
 class Book {
-
+    @Override
+    public String toString() {
+        return "Это просто книга";
+    }
 }
