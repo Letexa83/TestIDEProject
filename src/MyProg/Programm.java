a1eb93eec5cae220dcb59f41c489762d3277df51
Kondyakov <letexa83@gmail.com>
2020-08-11 16:57:50 +0300

    Tak nado

--- src/MyProg/Programm.java
+++ src/MyProg/Programm.java
@@ -4,6 +4,11 @@ public class Programm {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello world");
+        Book book = new Book();
 
     }
+}
+
+class Book {
+
 }
