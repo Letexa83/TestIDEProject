0455d47dd69fdf5f215ec205e7ea9c1d6c9811b8
Кондяков Евгений Анатольевич <ekondyakov@td-bf.ru>
2020-08-11 16:49:36 +0300

    Initial commit

---
+++ src/MyProg/Programm.java
@@ -0,0 +1,9 @@
+package MyProg;
+
+public class Programm {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+
+    }
+}
